db517afca7d63b6ffff26f73b64d556161e112ad
NubeDev <aidan@enviroservices.com.au>
2021-11-16 19:07:48 +1100

    clean up

--- frontend/lib/screen/detail_widget.dart
+++ frontend/lib/screen/detail_widget.dart
@@ -4,33 +4,36 @@ import 'package:frontend/services/api.dart';
 
 import 'edit_screen.dart';
 
-
 class DetailWidget extends StatefulWidget {
   DetailWidget(this.product);
+
   final Product product;
-
-
 
   @override
   _DetailWidgetState createState() => _DetailWidgetState();
 }
 
 class _DetailWidgetState extends State<DetailWidget> {
-
   final ApiService apiService = ApiService();
 
-
+  List<String> _locations = ['1', '2', '2', 'D']; // Option 2
+  String? _selectedLocation; // Option 2
+  late Future <Product> productList;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Details'),
+        title: Text('DEVICE: ${widget.product.name.toString()}'),
         actions: [
-          IconButton(onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>EditScreen(widget.product)));
-          }, icon: Icon(Icons.edit)),
-
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => EditScreen(widget.product)));
+              },
+              icon: Icon(Icons.edit)),
         ],
       ),
       body: SingleChildScrollView(
@@ -39,13 +42,57 @@ class _DetailWidgetState extends State<DetailWidget> {
           child: Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  ElevatedButton(
+                    child: Text("LogIn"),
+                    onPressed: () { print(111);},
+                  ),
+                  SizedBox(width: 5),
+                  DropdownButton(
+                    hint: Text('Please choose a location'), // Not necessary for Option 1
+                    value: _selectedLocation,
+                    onChanged: (newValue) {
+                      setState(() {
+                        _selectedLocation = newValue.toString();
+                        print(_selectedLocation);
+                        // getProductById("1");
+                        try {
+                          productList = apiService.getProductById(_selectedLocation.toString());
+                          productList.then((value) => {
+                            print(value.name)
+                          });
+                        } catch (e) {
+                          print(e);
+                        }
+
+                      });
+                    },
+                    items: _locations.map((location) {
+                      return DropdownMenuItem(
+                        child: new Text(location),
+                        value: location,
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
                 children: [
-                  Text('Product Name: '),
-                  SizedBox(width: 20,),
+                  Text('Product Name2: '),
+                  SizedBox(
+                    width: 20,
+                  ),
                   Text('${widget.product.name}')
                 ],
               ),
-              SizedBox(height: 20,),
+              SizedBox(
+                height: 20,
+              ),
+
               // Image.network('${widget.product.image}')
             ],
           ),
@@ -53,5 +100,4 @@ class _DetailWidgetState extends State<DetailWidget> {
       ),
     );
   }
-
 }
