82f4010ee6e0c1ef596928f038035d561aff839d
NubeDev <aidan@enviroservices.com.au>
2021-12-02 12:08:29 +1100

    updates

--- backend/app.go
+++ backend/app.go
@@ -2,29 +2,34 @@ package main
 
 import (
 	"fmt"
+	"github.com/NubeIO/rubix-updater/pkg/config"
+	"github.com/NubeIO/rubix-updater/pkg/database"
+	"github.com/NubeIO/rubix-updater/pkg/logger"
+	"github.com/NubeIO/rubix-updater/pkg/router"
+	"github.com/spf13/viper"
 )
 
 
 
 func main() {
 	fmt.Println("try and start")
-	//if err := config.Setup(); err != nil {
-	//	logger.Fatalf("config.Setup() error: %s", err)
-	//}
-	//
-	//if err := database.Setup(); err != nil {
-	//	logger.Fatalf("database.Setup() error: %s", err)
-	//}
-	//
-	//db := database.GetDB()
-	//r := router.Setup(db)
-	//
-	//host := "127.0.0.1"
-	//if h := viper.GetString("server.host"); h != "" {
-	//	host = h
-	//}
-	//logger.Infof("Server is starting at %s:%s", host, viper.GetString("server.port"))
-	//fmt.Printf("server is running at %s:%s Check logs for details\n", host, viper.GetString("server.port"))
-	//fmt.Println()
-	//logger.Fatalf("%v", r.Run(host+":"+viper.GetString("server.port")))
+	if err := config.Setup(); err != nil {
+		logger.Fatalf("config.Setup() error: %s", err)
+	}
+
+	if err := database.Setup(); err != nil {
+		logger.Fatalf("database.Setup() error: %s", err)
+	}
+
+	db := database.GetDB()
+	r := router.Setup(db)
+
+	host := "127.0.0.1"
+	if h := viper.GetString("server.host"); h != "" {
+		host = h
+	}
+	logger.Infof("Server is starting at %s:%s", host, viper.GetString("server.port"))
+	fmt.Printf("server is running at %s:%s Check logs for details\n", host, viper.GetString("server.port"))
+	fmt.Println()
+	logger.Fatalf("%v", r.Run(host+":"+viper.GetString("server.port")))
 }
