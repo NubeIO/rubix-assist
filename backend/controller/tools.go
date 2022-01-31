3eadc66f98306f645c8c6892e38d9d33ba20fb82
NubeDev <aidan@enviroservices.com.au>
2022-01-31 17:03:17 +1100

    updates

--- backend/controller/tools.go
+++ backend/controller/tools.go
@@ -7,6 +7,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func (base *Controller) ToolsGetArch(ctx *gin.Context) {
+	host, _, err := base.resolveHost(ctx)
+	if err != nil {
+		reposeHandler(nil, err, ctx)
+		return
+	}
+	h, err := base.hostCopy(host)
+	_host := admin.Admin{
+		Host: h,
+	}
+	arch, _, err := _host.DetectArch()
+	if err != nil {
+		reposeHandler(nil, errors.New("error on check host"), ctx)
+		return
+	}
+	reposeHandler(arch, nil, ctx)
+}
+
 func (base *Controller) ToolsEndPoints(ctx *gin.Context) {
 	host, _, err := base.resolveHost(ctx)
 	if err != nil {
@@ -27,5 +45,4 @@ func (base *Controller) ToolsEndPoints(ctx *gin.Context) {
 	} else {
 		reposeHandler(schema.GetToolsEndPointsSchema(), nil, ctx)
 	}
-
 }
