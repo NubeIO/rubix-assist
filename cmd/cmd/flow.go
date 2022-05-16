11dcc9e87078935653743caf89a02b33fd70a2a4
NubeDev <aidan@enviroservices.com.au>
2022-05-16 14:58:06 +1000

    added all cmd/cli for systemctl

--- cmd/cmd/flow.go
+++ cmd/cmd/flow.go
@@ -7,6 +7,7 @@ import (
 
 var (
 	flowScan bool
+	ipRange  string
 )
 
 var flowCmd = &cobra.Command{
@@ -17,9 +18,8 @@ var flowCmd = &cobra.Command{
 }
 
 func runFlow(cmd *cobra.Command, args []string) {
-
 	if flowScan {
-		scan := &flow.Scan{Debug: true}
+		scan := &flow.Scan{IP: ipRange, Iface: iface, Debug: true}
 		scan.Scan()
 	}
 
@@ -28,5 +28,6 @@ func runFlow(cmd *cobra.Command, args []string) {
 func init() {
 	RootCmd.AddCommand(flowCmd)
 	flowCmd.Flags().BoolVarP(&flowScan, "scan", "", false, "do a network scan for iot devices")
+	flowCmd.Flags().StringVarP(&ipRange, "ip-range", "", "192.168.15.1-254", "scan range ip example is 192.168.15.1-10 would scan the 10 ips")
 
 }
--- drivers/flow/scan.go
+++ drivers/flow/scan.go
@@ -1,8 +1,6 @@
 package flow
 
 import (
-	"fmt"
-	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/networking/networking"
 	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/networking/portscanner"
 )
 
@@ -12,32 +10,11 @@ type Scan struct {
 }
 
 func (s *Scan) Scan() (hosts *portscanner.Hosts) {
-	nets := networking.NewNets()
-	interfaceName := s.Iface
 	ip := s.IP
 	if ip == "" {
-		if interfaceName == "" { // if nothing is provided then take a guess of the user network
-			gateway, err := nets.GetNetworksThatHaveGateway()
-			if err != nil {
-				//responseHandler(nil, err, ctx)
-				return
-			}
-			for i, net := range gateway {
-				if i == 0 {
-					interfaceName = net.Interface
-				}
-			}
-			net, err := nets.GetNetworkByIface(interfaceName)
-			if err != nil {
-				//responseHandler(nil, err, ctx)
-			}
-			ip = net.Gateway
-		}
-	} else {
-		ip = "192.168.15"
+		ip = "192.168.15.1-254"
 	}
 
-	ip = fmt.Sprintf("%s-254", ip)
 	ports := []string{"22", "1313", "1414", "1616", "1615", "502", "1883"}
 
 	// IP sequence is defined by a '-' between first and last IP address .
