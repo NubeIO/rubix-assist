3c4207aa12cc0ed7c92b3536e747a2326cb171b6
NubeDev <aidan@enviroservices.com.au>
2022-05-26 05:42:08 +1000

    added host network

---
+++ controller/network.go
@@ -0,0 +1,68 @@
+package controller
+
+import (
+	"github.com/NubeIO/rubix-assist/model"
+	"github.com/gin-gonic/gin"
+)
+
+func getNetworkBody(ctx *gin.Context) (dto *model.Network, err error) {
+	err = ctx.ShouldBindJSON(&dto)
+	return dto, err
+}
+
+func (inst *Controller) GetHostNetwork(c *gin.Context) {
+	host, err := inst.DB.GetHostNetworkByName(c.Params.ByName("uuid"), true)
+	if err != nil {
+		reposeHandler(nil, err, c)
+		return
+	}
+	reposeHandler(host, err, c)
+}
+
+func (inst *Controller) GetHostNetworks(c *gin.Context) {
+	hosts, err := inst.DB.GetHostNetworks()
+	if err != nil {
+		reposeHandler(nil, err, c)
+		return
+	}
+	reposeHandler(hosts, err, c)
+}
+
+func (inst *Controller) CreateHostNetwork(c *gin.Context) {
+	m := new(model.Network)
+	err = c.ShouldBindJSON(&m)
+	host, err := inst.DB.CreateHostNetwork(m)
+	if err != nil {
+		reposeHandler(nil, err, c)
+		return
+	}
+	reposeHandler(host, err, c)
+}
+
+func (inst *Controller) UpdateHostNetwork(c *gin.Context) {
+	body, _ := getNetworkBody(c)
+	host, err := inst.DB.UpdateHostNetwork(c.Params.ByName("uuid"), body)
+	if err != nil {
+		reposeHandler(nil, err, c)
+		return
+	}
+	reposeHandler(host, err, c)
+}
+
+func (inst *Controller) DeleteHostNetwork(c *gin.Context) {
+	q, err := inst.DB.DeleteHostNetwork(c.Params.ByName("uuid"))
+	if err != nil {
+		reposeHandler(nil, err, c)
+	} else {
+		reposeHandler(q, err, c)
+	}
+}
+
+func (inst *Controller) DropHostNetworks(c *gin.Context) {
+	host, err := inst.DB.DropHostNetworks()
+	if err != nil {
+		reposeHandler(nil, err, c)
+		return
+	}
+	reposeHandler(host, err, c)
+}
--- database/networks.go
+++ database/networks.go
@@ -2,32 +2,31 @@ package dbase
 
 import (
 	"errors"
-	"github.com/NubeIO/nubeio-rubix-lib-helpers-go/pkg/nils"
 	"github.com/NubeIO/rubix-assist/model"
 	"github.com/NubeIO/rubix-assist/pkg/config"
 	"github.com/NubeIO/rubix-assist/pkg/logger"
 )
 
-func (d *DB) GetHosts() ([]*model.Host, error) {
-	var m []*model.Host
-	if err := d.DB.Find(&m).Error; err != nil {
+func (d *DB) GetHostNetworks() ([]*model.Network, error) {
+	var m []*model.Network
+	if err := d.DB.Preload("Hosts").Find(&m).Error; err != nil {
 		return nil, err
 	} else {
 		return m, nil
 	}
 }
 
-func (d *DB) GetHostByName(name string, isUUID bool) (*model.Host, error) {
-	m := new(model.Host)
+func (d *DB) GetHostNetworkByName(name string, isUUID bool) (*model.Network, error) {
+	m := new(model.Network)
 	switch isUUID {
 	case true:
-		if err := d.DB.Where("uuid = ? ", name).First(&m).Error; err != nil {
+		if err := d.DB.Where("uuid = ? ", name).Preload("Hosts").First(&m).Error; err != nil {
 			logger.Errorf("GetHost error: %v", err)
 			return nil, err
 		}
 		return m, nil
 	case false:
-		if err := d.DB.Where("name = ? ", name).First(&m).Error; err != nil {
+		if err := d.DB.Where("name = ? ", name).Preload("Hosts").First(&m).Error; err != nil {
 			logger.Errorf("GetHost error: %v", err)
 			return nil, err
 		}
@@ -37,34 +36,15 @@ func (d *DB) GetHostByName(name string, isUUID bool) (*model.Host, error) {
 	}
 }
 
-func (d *DB) CreateHost(host *model.Host) (*model.Host, error) {
+func (d *DB) CreateHostNetwork(host *model.Network) (*model.Network, error) {
 	if host.Name == "" {
 		host.Name = "RC"
 	}
-
-	existingHost, _ := d.GetHostByName(host.Name, false)
-	if existingHost != nil {
-		return nil, errors.New("a host with this name exists")
-	}
+	//existingHost, _ := d.GetHostByName(host.Name, false)
+	//if existingHost != nil {
+	//	return nil, errors.New("a host with this name exists")
+	//}
 	host.UUID = config.MakeTopicUUID(model.CommonNaming.Host)
-	if host.PingEnable == nil {
-		host.PingEnable = nils.NewFalse()
-	}
-	if host.IsLocalhost == nil {
-		host.IsLocalhost = nils.NewFalse()
-	}
-	if host.HTTPS == nil {
-		host.HTTPS = nils.NewFalse()
-	}
-	if host.Port == 0 {
-		host.Port = 22
-	}
-	if host.RubixPort == 0 {
-		host.RubixPort = 1616
-	}
-	if host.BiosPort == 0 {
-		host.BiosPort = 1617
-	}
 	if err := d.DB.Create(&host).Error; err != nil {
 		return nil, err
 	} else {
@@ -72,8 +52,8 @@ func (d *DB) CreateHost(host *model.Host) (*model.Host, error) {
 	}
 }
 
-func (d *DB) UpdateHostByName(name string, host *model.Host) (*model.Host, error) {
-	m := new(model.Host)
+func (d *DB) UpdateHostNetworkByName(name string, host *model.Network) (*model.Network, error) {
+	m := new(model.Network)
 	query := d.DB.Where("name = ?", name).Find(&m).Updates(host)
 	if query.Error != nil {
 		return nil, query.Error
@@ -82,8 +62,8 @@ func (d *DB) UpdateHostByName(name string, host *model.Host) (*model.Host, error
 	}
 }
 
-func (d *DB) UpdateHost(uuid string, host *model.Host) (*model.Host, error) {
-	m := new(model.Host)
+func (d *DB) UpdateHostNetwork(uuid string, host *model.Network) (*model.Network, error) {
+	m := new(model.Network)
 	query := d.DB.Where("uuid = ?", uuid).Find(&m).Updates(host)
 	if query.Error != nil {
 		return nil, query.Error
@@ -92,7 +72,7 @@ func (d *DB) UpdateHost(uuid string, host *model.Host) (*model.Host, error) {
 	}
 }
 
-func (d *DB) DeleteHost(uuid string) (ok bool, err error) {
+func (d *DB) DeleteHostNetwork(uuid string) (ok bool, err error) {
 	var m *model.Network
 	query := d.DB.Where("uuid = ? ", uuid).Delete(&m)
 	if query.Error != nil {
@@ -105,9 +85,9 @@ func (d *DB) DeleteHost(uuid string) (ok bool, err error) {
 	return true, nil
 }
 
-// DropHosts delete all.
-func (d *DB) DropHosts() (bool, error) {
-	var m *model.Host
+// DropHostNetworks delete all.
+func (d *DB) DropHostNetworks() (bool, error) {
+	var m *model.Network
 	query := d.DB.Where("1 = 1")
 	query.Delete(&m)
 	if query.Error != nil {
