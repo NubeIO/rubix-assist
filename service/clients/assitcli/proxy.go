fea50db9a49abd0db7b43c3246afaf083c23f4e6
NubeDev <aidan@enviroservices.com.au>
2022-06-25 07:18:07 +1000

    added body to proxies

--- service/clients/assitcli/proxy.go
+++ service/clients/assitcli/proxy.go
@@ -16,30 +16,33 @@ func (inst *Client) ProxyGET(hostID, path string) (*resty.Response, error) {
 	return resp, err
 }
 
-func (inst *Client) ProxyPOST(hostID, path string) (*resty.Response, error) {
+func (inst *Client) ProxyPOST(hostID, path string, body interface{}) (*resty.Response, error) {
 	path = fmt.Sprintf("proxy/%s", path)
 	path = strings.Replace(path, "//", "/", -1) // makes this /api//host/123 => /api/host/123
 	resp, err := inst.Rest.R().
+		SetBody(body).
 		SetHeader("host_uuid", hostID).
 		SetHeader("host_name", hostID).
 		Post(path)
 	return resp, err
 }
 
-func (inst *Client) ProxyPATCH(hostID, path string) (*resty.Response, error) {
+func (inst *Client) ProxyPATCH(hostID, path string, body interface{}) (*resty.Response, error) {
 	path = fmt.Sprintf("proxy/%s", path)
 	path = strings.Replace(path, "//", "/", -1) // makes this /api//host/123 => /api/host/123
 	resp, err := inst.Rest.R().
+		SetBody(body).
 		SetHeader("host_uuid", hostID).
 		SetHeader("host_name", hostID).
 		Patch(path)
 	return resp, err
 }
 
-func (inst *Client) ProxyPUT(hostID, path string) (*resty.Response, error) {
+func (inst *Client) ProxyPUT(hostID, path string, body interface{}) (*resty.Response, error) {
 	path = fmt.Sprintf("proxy/%s", path)
 	path = strings.Replace(path, "//", "/", -1) // makes this /api//host/123 => /api/host/123
 	resp, err := inst.Rest.R().
+		SetBody(body).
 		SetHeader("host_uuid", hostID).
 		SetHeader("host_name", hostID).
 		Put(path)
