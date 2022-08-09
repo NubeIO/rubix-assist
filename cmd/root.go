188972b1543487ea41be6730a39805082a50f893
enjuthulung <enjuthulung@gmail.com>
2022-08-09 13:32:22 +0545

    Make installable

--- cmd/root.go
+++ cmd/root.go
@@ -1,19 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-	"github.com/NubeIO/rubix-assist/pkg/config"
-	"github.com/NubeIO/rubix-assist/pkg/database"
-	"github.com/NubeIO/rubix-assist/pkg/logger"
-	"github.com/NubeIO/rubix-assist/pkg/router"
-	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"log"
 )
 
 var (
-	//model.Host
+	// model.Host
 	hostName     string
 	hostIP       string
 	hostPort     int
@@ -32,41 +24,16 @@ var RootCmd = &cobra.Command{
 	Use:   "nube-cli",
 	Short: "description",
 	Long:  `description`,
-	Run:   runServer,
 }
 
 var flgRoot struct {
-	startApp bool
-}
-
-func setup() {
-	logger.Init()
-	logger.SetLogLevel(logrus.InfoLevel)
-	logger.InfoLn("try and start rubix-updater")
-	if err := config.Setup(); err != nil {
-		logger.Errorf("config.Setup() error: %s", err)
-	}
-	if err := database.Setup(); err != nil {
-		logger.Fatalf("database.Setup() error: %s", err)
-	}
-
-}
-
-func runServer(cmd *cobra.Command, args []string) {
-
-	if flgRoot.startApp {
-		setup()
-		db := database.GetDB()
-		r := router.Setup(db)
-		r.MaxMultipartMemory = 250 << 20 //250 mb
-		host := "0.0.0.0"
-		if h := viper.GetString("server.host"); h != "" {
-			host = h
-		}
-		logger.Infof("Server is starting at %s:%s", host, viper.GetString("server.port"))
-		fmt.Printf("server is running at %s:%s Check logs for details\n", host, viper.GetString("server.port"))
-		log.Fatalf("%v", r.Run(host+":"+viper.GetString("server.port")))
-	}
+	startApp  bool
+	prod      bool
+	port      int
+	rootDir   string
+	appDir    string
+	dataDir   string
+	configDir string
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -77,10 +44,15 @@ func Execute() {
 }
 
 func init() {
-	RootCmd.Flags().BoolVarP(&flgRoot.startApp, "server", "", false, "start rubix assist")
+	RootCmd.PersistentFlags().BoolVarP(&flgRoot.prod, "prod", "", false, "prod")
+	RootCmd.PersistentFlags().IntVarP(&flgRoot.port, "port", "p", 1662, "port (default 1661)")
+	RootCmd.PersistentFlags().StringVarP(&flgRoot.rootDir, "root-dir", "r", "./", "root dir") // in production it will be `/data`
+	RootCmd.PersistentFlags().StringVarP(&flgRoot.appDir, "app-dir", "a", "./", "global dir") // in production it will be `/edge`
+	RootCmd.PersistentFlags().StringVarP(&flgRoot.dataDir, "data-dir", "d", "data", "data dir")
+	RootCmd.PersistentFlags().StringVarP(&flgRoot.configDir, "config-dir", "c", "config", "config dir")
 	RootCmd.PersistentFlags().StringVarP(&hostName, "host", "", "rc", "host name (default rc)")
 	RootCmd.PersistentFlags().StringVarP(&hostIP, "ip", "", "192.168.15.10", "host ip (default 192.168.15.10)")
-	RootCmd.PersistentFlags().IntVarP(&hostPort, "port", "", 22, "SSH Port")
+	RootCmd.PersistentFlags().IntVarP(&hostPort, "ssh-port", "", 22, "SSH Port")
 	RootCmd.PersistentFlags().StringVarP(&iface, "iface", "", "", "pc or host network interface example: eth0")
 	RootCmd.PersistentFlags().StringVarP(&hostUsername, "host-user", "", "pi", "host/linux username (default pi)")
 	RootCmd.PersistentFlags().StringVarP(&hostPassword, "host-pass", "", "N00BRCRC", "host/linux password")
---
+++ cmd/server.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/NubeIO/rubix-assist/pkg/config"
+	"github.com/NubeIO/rubix-assist/pkg/database"
+	"github.com/NubeIO/rubix-assist/pkg/logger"
+	"github.com/NubeIO/rubix-assist/pkg/router"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+var serverCmd = &cobra.Command{
+	Use:   "server",
+	Short: "server",
+	Long:  "",
+	Run:   runServer,
+}
+
+func runServer(cmd *cobra.Command, args []string) {
+	setup()
+	db := database.GetDB()
+	r := router.Setup(db)
+	r.MaxMultipartMemory = 250 << 20 // 250 mb
+
+	host := "0.0.0.0"
+	port := config.Config.GetPort()
+	logger.Infof("Server is starting at %s:%s", host, port)
+	fmt.Printf("server is running at %s:%s Check logs for details\n", host, port)
+	log.Fatalf("%v", r.Run(host+":"+viper.GetString("server.port")))
+}
+
+func init() {
+	RootCmd.AddCommand(serverCmd)
+}
+
+func setup() {
+	logger.Init()
+	logger.SetLogLevel(logrus.InfoLevel)
+	logger.InfoLn("try and start rubix-updater")
+	if err := config.Setup(RootCmd); err != nil {
+		logger.Errorf("config.Setup() error: %s", err)
+	}
+	if err := os.MkdirAll(config.Config.GetAbsDataDir(), 0755); err != nil {
+		panic(err)
+	}
+	if err := database.Setup(); err != nil {
+		logger.Fatalf("database.Setup() error: %s", err)
+	}
+}
