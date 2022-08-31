0067808dfcf111fd69402a8a0350d54e0a76310b
NubeDev <aidan@enviroservices.com.au>
2022-08-31 17:33:17 +1000

    fix bacnet api

--- service/clients/assitcli/ffbacnet.go
+++ service/clients/assitcli/ffbacnet.go
@@ -17,7 +17,7 @@ const bacnetMaster = "bacnetmaster"
 
 // BacnetWhoIs do a whois on an existing network
 func (inst *Client) BacnetWhoIs(hostIDName string, body *WhoIsOpts, networkUUID string, addDevices bool) ([]model.Device, error) {
-	url := fmt.Sprintf("proxy/ff/api/api/plugins/api/%s/whois/%s?add_devices=%t", bacnetMaster, networkUUID, addDevices)
+	url := fmt.Sprintf("proxy/ff/api/plugins/api/%s/whois/%s?add_devices=%t", bacnetMaster, networkUUID, addDevices)
 	resp, err := nresty.FormatRestyResponse(inst.Rest.R().
 		SetHeader("host_uuid", hostIDName).
 		SetHeader("host_name", hostIDName).
@@ -33,7 +33,7 @@ func (inst *Client) BacnetWhoIs(hostIDName string, body *WhoIsOpts, networkUUID 
 
 // BacnetDevicePoints get points from an added device
 func (inst *Client) BacnetDevicePoints(hostIDName, deviceUUID string, addPoints, makeWriteable bool) ([]model.Point, error) {
-	url := fmt.Sprintf("proxy/ff/api/api/plugins/api/%s/device/points/%s?add_points=%t&writeable_points=%t", bacnetMaster, deviceUUID, addPoints, makeWriteable)
+	url := fmt.Sprintf("proxy/ff/api/plugins/api/%s/device/points/%s?add_points=%t&writeable_points=%t", bacnetMaster, deviceUUID, addPoints, makeWriteable)
 	resp, err := nresty.FormatRestyResponse(inst.Rest.R().
 		SetHeader("host_uuid", hostIDName).
 		SetHeader("host_name", hostIDName).
