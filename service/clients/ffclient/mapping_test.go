e4ddfc1a5a7d147f8a165920c233764eb721bbfa
NubeDev <aidan@enviroservices.com.au>
2022-10-31 14:54:28 +1100

    added remote mapping apis

--- service/clients/ffclient/mapping_test.go
+++ service/clients/ffclient/mapping_test.go
@@ -7,37 +7,34 @@ import (
 )
 
 var cli = NewLocalClient(&Connection{
-	Ip:   "0.0.0.0",
+	Ip:   "123.209.193.75",
 	Port: 1660,
 })
 
 var mapping = &Mapping{
-	EdgeDeviceName:    "bgis-test",
-	FlowNetworkUUID:   "fln_2c7d8c775574461b",
-	NetworkUUID:       "net_4896cac168a24bf1",
-	NetworkUUIDRemote: "net_bbf64ecb6f694f0b",
+	EdgeDeviceName:    "RAAF Richmond 3",
+	FlowNetworkUUID:   "fln_b43baddc03bf4590",
+	NetworkUUID:       "net_f8f05815339b4ff9",
+	NetworkUUIDRemote: "net_aaf0666fdbe5429b",
 }
 
 // MAKE REMOTE NETWORKS
 
 func TestFlowClient_MakeRemoteConsumers(t *testing.T) {
-
 	err := cli.MakeRemoteConsumers(mapping)
-	//
 	fmt.Println(err)
 
 }
 
+// make all the remote points
 func TestFlowClient_MakeRemoteDevicePoints(t *testing.T) {
-
 	err := cli.MakeRemoteDevicePoints(mapping)
-	//
 	fmt.Println(err)
 
 }
 
 // MAKE LOCAL STREAMS
-
+// make all the streams
 func TestFlowClient_MakeLocalStreams(t *testing.T) {
 	err := cli.MakeLocalStreams(mapping)
 	fmt.Println(err)
