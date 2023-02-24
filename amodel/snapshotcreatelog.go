0fb242f51831a0630aed0909d48e599e4c73bfea
enjuthulung <enjuthulung@gmail.com>
2023-02-24 17:18:02 +0545

    Add patch api endpoint and description field for snapshot create and restore logs

--- amodel/snapshotcreatelog.go
+++ amodel/snapshotcreatelog.go
@@ -11,9 +11,10 @@ const (
 )
 
 type SnapshotCreateLog struct {
-	UUID      string       `json:"uuid" gorm:"primary_key" get:"true" delete:"true"`
-	HostUUID  string       `json:"host_uuid" get:"true" post:"true"`
-	Msg       string       `json:"msg" get:"true" post:"true" patch:"true"`
-	Status    CreateStatus `json:"status" get:"true" post:"true" patch:"true"`
-	CreatedAt time.Time    `json:"created_at" get:"true"`
+	UUID        string       `json:"uuid" gorm:"primary_key" get:"true" delete:"true"`
+	HostUUID    string       `json:"host_uuid" get:"true" post:"true"`
+	Msg         string       `json:"msg" get:"true" post:"true" patch:"true"`
+	Status      CreateStatus `json:"status" get:"true" post:"true" patch:"true"`
+	Description string       `json:"description" get:"true" post:"true" patch:"true"`
+	CreatedAt   time.Time    `json:"created_at" get:"true"`
 }
--- amodel/snapshotrestorelog.go
+++ amodel/snapshotrestorelog.go
@@ -11,9 +11,10 @@ const (
 )
 
 type SnapshotRestoreLog struct {
-	UUID      string        `json:"uuid" gorm:"primary_key" get:"true" delete:"true"`
-	HostUUID  string        `json:"host_uuid" get:"true" post:"true" patch:"true"`
-	Msg       string        `json:"msg" get:"true" post:"true" patch:"true"`
-	Status    RestoreStatus `json:"status" get:"true" post:"true" patch:"true"`
-	CreatedAt time.Time     `json:"created_at" get:"true"`
+	UUID        string        `json:"uuid" gorm:"primary_key" get:"true" delete:"true"`
+	HostUUID    string        `json:"host_uuid" get:"true" post:"true" patch:"true"`
+	Msg         string        `json:"msg" get:"true" post:"true" patch:"true"`
+	Status      RestoreStatus `json:"status" get:"true" post:"true" patch:"true"`
+	Description string        `json:"description" get:"true" post:"true" patch:"true"`
+	CreatedAt   time.Time     `json:"created_at" get:"true"`
 }
