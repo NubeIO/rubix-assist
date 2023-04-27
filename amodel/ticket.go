a2060655a29c6ac3a9810bb60049b00afea8f5b1
NubeDev <aidan@enviroservices.com.au>
2023-04-28 09:21:28 +1000

    added base code for ticket system

--- amodel/ticket.go
+++ amodel/ticket.go
@@ -25,24 +25,25 @@ const (
 
 type Comment struct {
 	UUID       string    `json:"uuid" gorm:"primary_key"`
-	CreatedAt  time.Time `json:"created_at"`
-	ModifiedAt time.Time `json:"modified_at"`
-	TicketID   string    `json:"ticket_id"`
+	TicketID   string    `json:"ticket_id"  gorm:"TYPE:string REFERENCES tickets;"`
 	Owner      string    `json:"owner"`
 	Content    string    `json:"content"`
 	Metadata   string    `json:"metadata"`
+	CreatedAt  time.Time `json:"created_at"`
+	ModifiedAt time.Time `json:"modified_at"`
 }
 
 type Ticket struct {
 	UUID            string                `json:"uuid" gorm:"primary_key"`
-	CreatedAt       time.Time             `json:"created_at"`
-	ModifiedAt      time.Time             `json:"modified_at"`
 	Issuer          string                `json:"issuer"`
 	Owner           string                `json:"owner"`
 	Subject         string                `json:"subject"`
 	Content         string                `json:"content"`
 	Metadata        string                `json:"metadata"`
+	HostUUID        string                `json:"hostUUID"`
 	ImportanceLevel TicketImportanceLevel `json:"importance_level"`
 	Status          TicketStatus          `json:"status"`
 	Comments        []*Comment            `json:"comments"`
+	CreatedAt       time.Time             `json:"created_at"`
+	ModifiedAt      time.Time             `json:"modified_at"`
 }
